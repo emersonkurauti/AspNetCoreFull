fbbea0696815fa06cab0f597d21c5b15073191d0
emerson <emersonkurauti@gmail.com>
2020-08-11 19:53:48 -0300

    Implementação de testes

---
+++ RallyDakar.Dominio.Test/Temporadas/AdicionarDuasEquipesTeste.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RallyDakar.Dominio.Entidades;
+
+namespace RallyDakar.Dominio.Teste.Temporadas
+{
+    [TestClass]
+    public class AdicionarDuasEquipesTeste
+    {
+        Temporada temporada;
+        Equipe equipe1;
+        Equipe equipe2;
+        Equipe equipe3;
+        Equipe equipe4;
+
+        [TestInitialize]
+        public void Inicializacao()
+        {
+            temporada = new Temporada();
+            temporada.Id = 1;
+            temporada.Nome = "Temporada 1";
+
+            equipe1 = new Equipe();
+            equipe1.Id = 1;
+            equipe1.Nome = "Teste1";
+
+            equipe2 = new Equipe();
+            equipe2.Id = 2;
+            equipe2.Nome = "Teste2";
+
+            equipe3 = null;
+
+            equipe4 = new Equipe();
+            equipe4.Id = 4;
+
+            temporada.AdicionarEquipe(equipe1);
+            temporada.AdicionarEquipe(equipe2);
+            temporada.AdicionarEquipe(equipe3);
+            temporada.AdicionarEquipe(equipe4);
+        }
+
+        [TestMethod]
+        public void DuasEquipesAdicionadasCorretamente()
+        {
+            Assert.IsTrue(temporada.Equipes.Count == 2);
+        }
+    }
+}
--- RallyDakar.Dominio/Entidades/Temporada.cs
+++ RallyDakar.Dominio/Entidades/Temporada.cs
@@ -9,6 +9,22 @@ namespace RallyDakar.Dominio.Entidades
         public string Nome { get; set; }
         public DateTime DataInicio { get; set; }
         public DateTime? DataFim { get; set; }
-        public ICollection<Equipe> Equipes { get; set; }
+        public virtual ICollection<Equipe> Equipes { get; set; }
+
+        public Temporada()
+        {
+            Equipes = new List<Equipe>();
+        }
+
+        public void AdicionarEquipe(Equipe equipe)
+        {
+            if (equipe == null)
+                return;
+
+            if (string.IsNullOrEmpty(equipe.Nome))
+                return;
+
+            Equipes.Add(equipe);
+        }
     }
 }
